e2e23520253222a719d0be7e64d84093c518268f
Polina <polinacherevatenko@gmail.com>
2026-04-28 10:02:49 -0400

    Initial Commit

---
+++ FishyFlopTests/FishyFlopTests.swift
@@ -0,0 +1,19 @@
+//
+//  FishyFlopTests.swift
+//  FishyFlopTests
+//
+//  Created by Polina on 2026-04-28.
+//
+
+import Testing
+@testable import FishyFlop
+
+struct FishyFlopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
